652cda4bf9f3c0e69c4557c355fc048d018755ff
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-14 12:16:32 +0530

    issue-153

--- issue-154/lambda.js
+++ issue-154/lambda.js
@@ -1,13 +1,11 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
-	ddb.update({
-		TableName: 'testHiru',
-		Key: { 'ID': '001' },
-		ExpressionAttributeNames: { '#sample': 'ID' },
-		ExpressionAttributeValues: { ':test': '001' },
-		UpdateExpression: 'set #sample = :test',
-		ConditionExpression: '#sample = :test',
+
+
+	ddb.put({
+		TableName: 'Hiru_DB',
+		Item: { 'ID': '001' }
 	}, function (err, data) {
 		if (err) {
 			//handle error
