4a61b46700450affc0163e7b24b2dddcac46dfff
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-14 13:55:20 +0530

    commit issue

--- issue-154/lambda.js
+++ issue-154/lambda.js
@@ -1,4 +1,6 @@
 let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
@@ -13,6 +15,23 @@ exports.handler = function (event, context, callback) {
 			//your logic goes here
 		}
 	});
+	sqs.sendMessage({
+		MessageBody: 'your message body should be assigned here test',
+		QueueUrl: 'https://sqs.ap-southeast-1.amazonaws.com/263248768798/test',
+		DelaySeconds: '0',
+		MessageAttributes: {
+			"undefined": {
+				"DataType": "undefined",
+				"BinaryValue": "undefined"
+			}
+		}
+	}, function (data) {
+		// your logic (logging etc) to handle successful message delivery, should be here
+	}, function (error) {
+		// your logic (logging etc) to handle failures, should be here
+	});
+
+
 
 
 	callback(null, 'Successfully executed');
